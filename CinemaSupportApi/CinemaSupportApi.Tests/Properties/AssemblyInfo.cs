029560b061d7b3dd60599d4d2f67ab543238205c
Filip Dobrowolski <filip.dobrowolski@outlook.com>
2018-11-11 10:18:17 +0100

    Renaming solution to ends with "api"

--- CinemaSupportApi/CinemaSupportApi.Tests/Properties/AssemblyInfo.cs
+++ CinemaSupportApi/CinemaSupportApi.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CinemaSupportApp")]
+[assembly: AssemblyTitle("CinemaSupportApi.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CinemaSupportApp")]
+[assembly: AssemblyProduct("CinemaSupportApi.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8c17e369-6c10-4c77-91e6-69379ee78891")]
+[assembly: Guid("bd15c9fd-11a8-4ad8-91c1-39a08276f65a")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
